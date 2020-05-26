cd3d13b17840eae43ab3beb9fc242f6eeea4f11a
cheanhminh98 <cheanhminh1401@gmail.com>
2020-05-26 13:35:14 +0700

    Create Auth

---
+++ laravel/resources/lang/vi/language.php
@@ -0,0 +1,14 @@
+<?php 
+return [
+    'email'=>'Địa chỉ Email',
+    'password'=>'Mật khẩu',
+    'remember_me'=>'Ghi nhớ đăng nhập',
+    'forgot_password'=>'Quên mật khẩu?',
+    'login'=>'Đăng nhập',
+    'register'=>'Đăng ký',
+    'name'=>'Tên tài khoản',
+    'confirm_password'=>'Xác nhận mật khẩu',
+    'reset_password'=>'Đặt lại mật khẩu',
+    'send_password_reset'=>'Gửi liên kết đặt lại mật khẩu',
+    'dashboard'=>'Bảng điều khiển',
+];
---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Intermediate</title>
+
+        
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+               
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
