5fe8c91aeac1b9021f31982d7e3aa597e48aa207
cheanhminh98 <cheanhminh1401@gmail.com>
2020-05-26 13:35:14 +0700

    Create Auth

---
+++ laravel/resources/lang/en/language.php
@@ -0,0 +1,14 @@
+<?php 
+return [
+    'email'=>'E-Mail Address',
+    'password'=>'Password',
+    'remember_me'=>'Remember Me',
+    'forgot_password'=>'Forgot Your Password?',
+    'login'=>'Login',
+    'register'=>'Register',
+    'name'=>'Name',
+    'confirm_password'=>'Confirm Password',
+    'reset_password'=>'Reset Password',
+    'send_password_reset'=>'Send Password Reset Link',
+    'dashboard'=>'Dashboard',
+];
